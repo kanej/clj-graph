64dcf48f8773032b38ce1c6765b537e64c6943a1
John Kane <john.kane@spartan-solutions.com>
2013-02-23 16:50:50 +0000

    Moved the functions out of the test namespace.

--- src/graph/core.clj
+++ src/graph/core.clj
@@ -1,6 +1,30 @@
 (ns graph.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn increment-2d-array [a]
+  (let [arrity (count a)
+        extend-existing-rows (vec (map #(conj % false) a))
+        append-new-row (conj extend-existing-rows (vec (repeat (inc arrity) false)))]
+    append-new-row))
+
+(defn new-graph []
+  {:nodes {} :edges []})
+
+(defn add-node [g node]
+  (let [old-nodes (:nodes g)
+        new-nodes (assoc old-nodes node (count old-nodes))
+        old-edges (:edges g)
+        new-edges (increment-2d-array old-edges)]
+    {:nodes new-nodes :edges new-edges}))
+
+(defn add-edge [g from-node to-node]
+  (let [from-node-index (get-in g [:nodes from-node])
+        to-node-index   (get-in g [:nodes to-node])]
+    (-> g 
+      (assoc-in [:edges from-node-index to-node-index] true)
+      (assoc-in [:edges to-node-index from-node-index] true)))) 
+
+(defn edge? [g from-node to-node]
+  (and (get-in g [:nodes from-node])
+       (get-in g [:nodes to-node])
+       (get-in g [:edges (get-in g [:nodes from-node]) (get-in g [:nodes to-node])])))
+
--- test/graph/core_test.clj
+++ test/graph/core_test.clj
@@ -2,34 +2,6 @@
   (:use clojure.test
         graph.core))
 
-(defn increment-2d-array [a]
-  (let [arrity (count a)
-        extend-existing-rows (vec (map #(conj % false) a))
-        append-new-row (conj extend-existing-rows (vec (repeat (inc arrity) false)))]
-    append-new-row))
-
-(defn new-graph []
-  {:nodes {} :edges []})
-
-(defn add-node [g node]
-  (let [old-nodes (:nodes g)
-        new-nodes (assoc old-nodes node (count old-nodes))
-        old-edges (:edges g)
-        new-edges (increment-2d-array old-edges)]
-    {:nodes new-nodes :edges new-edges}))
-
-(defn add-edge [g from-node to-node]
-  (let [from-node-index (get-in g [:nodes from-node])
-        to-node-index   (get-in g [:nodes to-node])]
-    (-> g 
-      (assoc-in [:edges from-node-index to-node-index] true)
-      (assoc-in [:edges to-node-index from-node-index] true)))) 
-
-(defn edge? [g from-node to-node]
-  (and (get-in g [:nodes from-node])
-       (get-in g [:nodes to-node])
-       (get-in g [:edges (get-in g [:nodes from-node]) (get-in g [:nodes to-node])])))
-
 (deftest arrays-of-2d
   (testing "Incrementing the array"
     (let [empty-array []]
@@ -71,3 +43,4 @@
       (is (not (edge? g :b :b)))
       (is (not (edge? g :c :c))))))
 
+;; neighbours
