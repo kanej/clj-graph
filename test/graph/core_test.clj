fddf40669250aa128d5e6339eb07aaecc1d6808f
John Kane <john.kane@spartan-solutions.com>
2013-03-03 22:34:20 +0000

    Some more explanation around the basic tests.

--- test/graph/core_test.clj
+++ test/graph/core_test.clj
@@ -17,28 +17,40 @@
       (is (= {:a 0} (:nodes (add-node g :a))))
       (is (= {:a 0 :b 1} (:nodes (add-node (add-node g :a) :b))))))
   (testing "can query for an edge"
+
     (let [g (-> (graph)
                 (add-node :a)
                 (add-node :b))]
+      ;; There are no edges until an edge is added 
       (is (not (edge? g :a :b)))
+      (is (not (edge? g :b :a)))
+      ;; Non-existant vertexes return false as well
       (is (not (edge? g :non-existant :b)))
       (is (not (edge? g :a :non-existant)))
-      (is (not (edge? g :b :a)))
+      ;; There is an edge once it has been added
       (is (edge? (add-edge g :a :b) :a :b)))
+
     (let [g (-> (graph)
                 (add-node :a)
                 (add-node :b)
                 (add-node :c)
                 (add-edge :a :b))] 
+
+      ;; Correctly detects the expected edges
       (is (edge? g :a :b))
       (is (edge? g :b :a))
 
+      ;; detects that there is no edge from a to c
+      ;; in either direction
       (is (not (edge? g :a :c)))
       (is (not (edge? g :c :a)))
 
+      ;; Similarly for b to c
       (is (not (edge? g :b :c)))
       (is (not (edge? g :c :b)))
 
+      ;; There should be no edges from a node
+      ;; to itself
       (is (not (edge? g :a :a)))
       (is (not (edge? g :b :b)))
       (is (not (edge? g :c :c))))))
