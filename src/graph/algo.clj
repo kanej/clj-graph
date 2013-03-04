0d66ded0c9d68dcc9f0b9377fabc84dbaa65fd76
John Kane <john.kane@spartan-solutions.com>
2013-03-04 22:12:16 +0000

    Reorganised and added doc strings.

--- src/graph/algo.clj
+++ src/graph/algo.clj
@@ -1,11 +1,15 @@
 (ns graph.algo
+  ^{:author "John Kane"
+    :description "Text book graph algorithms."}
   (:use graph.core))
 
-(defn- unseen-neighbours [graph vertex vertex-state]
-  (let [neighbours (neighbours graph vertex)]
-    (filter #(= :white (get vertex-state %)) neighbours)))
+(declare unseen-neighbours)
 
 (defn depth-first-search 
+  "Performs a depth first traversal on the given graph.
+  
+   Currently it only finds the vertexes in the connected
+   graph starting at the given vertex."
   ([graph]
     (let [vertex (first (keys (:nodes graph)))]
       (if (nil? vertex)
@@ -27,11 +31,18 @@
             next-vertexes (rest next-vertexes)
             updated-walk (conj walk current-vertex)
 
-            ;; Update the vertex state marking the current vertex as black
-            ;; and the neighbours as seen i.e. gray
-            updated-vertex-state (assoc vertex-state current-vertex :black)
-            updated-vertex-state (reduce #(assoc %1 %2 :gray) updated-vertex-state white-neighbours)
-
             ;; Push the unseen neighbours onto the next vertexes list
-            next-vertexes (concat white-neighbours next-vertexes)] 
+            next-vertexes (concat white-neighbours next-vertexes)
+
+            ;; Update the vertex state marking the current vertex as black
+            ;; and the neighbours as seen i.e. grey
+            updated-vertex-state (assoc vertex-state current-vertex :black)
+            updated-vertex-state (reduce #(assoc %1 %2 :grey) updated-vertex-state white-neighbours)] 
         (recur graph updated-vertex-state next-vertexes updated-walk)))))
+
+(defn- unseen-neighbours 
+  "Find the neighbours of a vertex that have noot been seen before
+   as defined by having a colour of White rather than Grey or Black."
+  [graph vertex vertex-state]
+  (let [neighbours (neighbours graph vertex)]
+    (filter #(= :white (get vertex-state %)) neighbours)))
